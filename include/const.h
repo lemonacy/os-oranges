0874187c7bbf016c6e094cf94049a4929b868fce
lemonacy <dengkuadong@gmail.com>
2019-02-18 17:09:50 +0800

    第七章 通过设置VGA寄存器实现光标跟随和Shift+Up向上滚屏

--- include/const.h
+++ include/const.h
@@ -27,10 +27,22 @@
 #define INT_S_CTL 0xA0     /* I/O port for interrupt controller - Slave */
 #define INT_S_CTLMASK 0xA1 /* setting bits in this port disables ints - Slave */
 
+/* VGA */
+#define	CRTC_ADDR_REG	0x3D4	/* CRT Controller Registers - Addr Register */
+#define	CRTC_DATA_REG	0x3D5	/* CRT Controller Registers - Data Register */
+#define	START_ADDR_H	0xC	/* reg index of video mem start addr (MSB) */
+#define	START_ADDR_L	0xD	/* reg index of video mem start addr (LSB) */
+#define	CURSOR_H	0xE	/* reg index of cursor position (MSB) */
+#define	CURSOR_L	0xF	/* reg index of cursor position (LSB) */
+#define	V_MEM_BASE	0xB8000	/* base of color video memory */
+#define	V_MEM_SIZE	0x8000	/* 32K: B8000H -> BFFFFH */
+
+/* Hardware interrupts */
 #define NR_IRQ 16 /* 对应主从两个8259A */
 #define CLOCK_IRQ 0
 #define KEYBOARD_IRQ 1
 
+/* system call */
 #define NR_SYS_CALL 1
 
 /* 8253/8254 PIT (Programmable Interval Timer) */
--- kernel/tty.c
+++ kernel/tty.c
@@ -1,6 +1,7 @@
 #include "const.h"
 #include "proto.h"
 #include "keyboard.h"
+#include "global.h"
 
 PUBLIC void task_tty()
 {
@@ -18,4 +19,38 @@ PUBLIC void in_process(u32 key)
         output[0] = key & 0xFF;
         disp_str(output);
     }
+    else
+    {
+        int raw_code = key & MASK_RAW;
+        switch (raw_code)
+        {
+        case UP:
+            if ((key & FLAG_SHIFT_L) || (key & FLAG_SHIFT_R))
+            {
+                disable_int();
+                out_byte(CRTC_ADDR_REG, START_ADDR_H);
+                out_byte(CRTC_DATA_REG, ((80 * 15) >> 8) & 0xFF);
+                out_byte(CRTC_ADDR_REG, START_ADDR_L);
+                out_byte(CRTC_DATA_REG, (80 * 15) & 0xFF);
+                enable_int();
+            }
+            break;
+        case DOWN:
+            if ((key & FLAG_SHIFT_L) || (key & FLAG_SHIFT_R))
+            {
+                /* Shift + Down, do nothing */
+            }
+            break;
+        default:
+            break;
+        }
+    }
+
+    /* 实现光标跟随 */
+    disable_int();
+    out_byte(CRTC_ADDR_REG, CURSOR_H);
+    out_byte(CRTC_DATA_REG, ((disp_pos / 2) >> 8) & 0xFF);
+    out_byte(CRTC_ADDR_REG, CURSOR_L);
+    out_byte(CRTC_DATA_REG, (disp_pos / 2) & 0xFF);
+    enable_int();
 }
