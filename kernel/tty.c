dc9a35d297f3376d0101c3a3018e97b446e8e9a5
lemonacy <dengkuadong@gmail.com>
2019-02-20 10:30:07 +0800

    第七章 处理Enter和BackSpace按键

--- kernel/tty.c
+++ kernel/tty.c
@@ -10,6 +10,7 @@
 PRIVATE void init_tty(TTY *p_tty);
 PRIVATE void tty_do_read(TTY *p_tty);
 PRIVATE void tty_do_write(TTY *p_tty);
+PRIVATE void put_key(TTY *p_tty, u32 key);
 
 PUBLIC void task_tty()
 {
@@ -44,22 +45,19 @@ PUBLIC void in_process(TTY *p_tty, u32 key)
 {
     if (!(key & FLAG_EXT)) // 如果是可打印字符，则丢到当前tty的inbuf缓冲区，然后tty_do_write从缓冲区读取显示
     {
-        if (p_tty->inbuf_count < TTY_IN_BYTES)
-        {
-            *(p_tty->p_inbuf_head) = key;
-            p_tty->p_inbuf_head++;
-            if (p_tty->p_inbuf_head == p_tty->in_buf + TTY_IN_BYTES)
-            {
-                p_tty->p_inbuf_head = p_tty->in_buf;
-            }
-            p_tty->inbuf_count++;
-        }
+        put_key(p_tty, key);
     }
     else
     {
         int raw_code = key & MASK_RAW;
         switch (raw_code)
         {
+        case ENTER:
+            put_key(p_tty, '\n');
+            break;
+        case BACKSPACE:
+            put_key(p_tty, '\b');
+            break;
         case UP:
             if ((key & FLAG_SHIFT_L) || (key & FLAG_SHIFT_R))
             {
@@ -117,5 +115,19 @@ PRIVATE void tty_do_write(TTY *p_tty)
         p_tty->inbuf_count--;
 
         out_char(p_tty->p_console, ch);
+    }
+}
+
+PRIVATE void put_key(TTY *p_tty, u32 key)
+{
+    if (p_tty->inbuf_count < TTY_IN_BYTES)
+    {
+        *(p_tty->p_inbuf_head) = key;
+        p_tty->p_inbuf_head++;
+        if (p_tty->p_inbuf_head == p_tty->in_buf + TTY_IN_BYTES)
+        {
+            p_tty->p_inbuf_head = p_tty->in_buf;
+        }
+        p_tty->inbuf_count++;
     }
 }
